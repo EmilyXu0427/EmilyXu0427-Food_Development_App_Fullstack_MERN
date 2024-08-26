05b65379b04ab075e66da1f0a4e7b31c4cb3e100
EmilyXu0427 <renbo.xu@usask.ca>
2024-08-26 11:03:08 -0700

    Adjust the output box to flexible display with scrollbar

--- frontend/src/pages/RecipeChatbot.jsx
+++ frontend/src/pages/RecipeChatbot.jsx
@@ -14,20 +14,23 @@ const RecipeChatbot = () => {
 
     return (
         <div className='bg-cover bg-center h-screen w-screen'
-        style={{ backgroundImage: `url(${chatBotBG})`}}>
-            <div className='flex flex-col text-xl font-sans font-semibold w-[600px] h-[800px] p-10 bg-red-200 mx-auto'>
-                    <span className='flex-grow bg-white border rounded-lg'>{answer}</span>
-                    <span className='pb-3 text-2xl pt-5'> Start chat with Recipe bot </span>
-                    <textarea 
+            style={{ backgroundImage: `url(${chatBotBG})` }}>
+            {/* Create parent container to adjust height and width of chatbot window */}
+            <div className='flex items-center justify-center min-h-screen'>
+                <div className='flex flex-col text-xl font-sans font-semibold w-[600px] h-[1000px] p-10 bg-red-200 mx-auto rounded-3xl'>
+                    {/* <span className='flex-grow bg-white border rounded-lg'>{answer}</span> */}
+                    <span className='block h-108 w-108 overflow-y-scroll bg-white border rounded-lg'>{answer}</span>
+                    <span className='pb-3 text-2xl pt-5'> Start chat with recipe chatbot </span>
+                    <textarea
                         onChange={(event) => setPrompt(event.target.value)}
                         className='border rounded border rounded-lg pb-4'>
                     </textarea>
-                    <br/>
+                    <br />
                     <button
                         onClick={() => getAnswer(prompt)}
                         className='bg-green-800 text-white border rounded-lg h-12'>Send</button>
+                </div>
             </div>
-
         </div>
     );
 };
